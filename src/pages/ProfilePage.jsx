a381d24086f726c66fe9639849875066b93fafc3
Abd-AllahHassan <aba252004@gmail.com>
2025-04-20 14:44:56 +0200

    deploy test

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -93,18 +93,6 @@ const ProfilePage = () => {
     return <Navigate to="/login" replace />;
   }
 
-  if (loading) {
-    return 
-    <>
-   <div className="flex justify-center items-center h-screen">
-      <motion.div
-        animate={{ rotate: 360 }}
-        transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
-        className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full"
-      />
-    </div>
-    </>;
-  }
 
   return (
     <>
